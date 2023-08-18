aadd6d821b5423891655c17184c5deb2a2e8c060
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-08-19 02:53:58 +0600

    Added Academic Faculty Route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,30 @@
 import express from 'express';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: '/academic-semesters',
-    route: AcademicSemesterRoutes,
+    path: "/academic-semesters",
+    route: AcademicSemesterRoutes
   },
+  {
+    path: '/academic-faculties',
+    route: academicFacultyRoutes
+  },
+  // {
+  //   path: '/academic-departments',
+  //   route: academicDepartmentRoutes
+  // },
+  // {
+  //   path: '/faculties',
+  //   route: facultyRoutes
+  // },
+  // {
+  //   path: '/students',
+  //   route: studentRoutes
+  // },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
